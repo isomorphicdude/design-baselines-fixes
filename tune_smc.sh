e77a75bee3082ab29013e9cfe5c1bc3a68898dc4
isomorphicdude <isometricat@icloud.com>
2024-10-30 13:57:36 +0000

    update for smcdiffopt ablation

--- tune_smc.sh
+++ tune_smc.sh
@@ -2,18 +2,59 @@
 
 TASK="Superconductor-RandomForest-v0"
 # TASK="TFBind8-Exact-v0"
-SCALING=1
-for SEED in 4 5; do
-# #     python design_baselines/smcdiffopt/__init__.py --seed $SEED --num-timesteps 100 --beta-scaling 10 --task "Superconductor-RandomForest-v0" --noise-sample-size 1
-#     # python design_baselines/smcdiffopt/__init__.py --seed $SEED --num-timesteps 100 --beta-scaling 100 --task "Superconductor-RandomForest-v0" --noise-sample-size 1
-    python design_baselines/smcdiffopt/__init__.py --seed $SEED --num-timesteps 100 --beta-scaling 10 --task $TASK  --anneal True --use-x0 False
-    python design_baselines/smcdiffopt/__init__.py --seed $SEED --num-timesteps 100 --beta-scaling 10 --task $TASK  --anneal False --use-x0 False
-    python design_baselines/smcdiffopt/__init__.py --seed $SEED --num-timesteps 100 --beta-scaling $SCALING --task $TASK  --anneal True --use-x0 True
-    python design_baselines/smcdiffopt/__init__.py --seed $SEED --num-timesteps 100 --beta-scaling $SCALING --task $TASK  --anneal False --use-x0 True
+METHOD="smcdiffopt"
+# SCALING=1
+for SEED in 10 20 30 40 50; do
+    python design_baselines/smcdiffopt/__init__.py \
+        --seed $SEED \
+        --num-timesteps 100 \
+        --beta-scaling 10 \
+        --task $TASK  \
+        --anneal False \
+        --noise-sample-size 1 \
+        --use-x0 True
+    python design_baselines/smcdiffopt/__init__.py \
+        --seed $SEED \
+        --num-timesteps 100 \
+        --beta-scaling 10 \
+        --task $TASK  \
+        --anneal False \
+        --noise-sample-size 1 \
+        --use-x0 False
+
+    python design_baselines/smcdiffopt/__init__.py \
+        --seed $SEED \
+        --num-timesteps 100 \
+        --beta-scaling 10 \
+        --task $TASK  \
+        --anneal True \
+        --noise-sample-size 1 \
+        --use-x0 True
+        
+    python design_baselines/smcdiffopt/__init__.py \
+        --seed $SEED \
+        --num-timesteps 100 \
+        --beta-scaling 10 \
+        --task $TASK  \
+        --anneal True \
+        --noise-sample-size 1 \
+        --use-x0 False
+
+    # python design_baselines/smcdiffopt/__init__.py \
+    #     --seed $SEED \
+    #     --num-timesteps 100 \
+    #     --beta-scaling $SCALING \
+    #     --task $TASK  \
+    #     --anneal False \
+    #     --use-x0 True \
+    #     --method "svdd" \
+    #     --noise-sample-size 100 \
+    #     --evaluation-samples 2
+
 #     # python design_baselines/smcdiffopt/__init__.py --retrain-model False  --task "TFBind8-Exact-v0" --no-task-relabel
     
 done
-python design_baselines/smcdiffopt/compute_scores.py --task $TASK
+python design_baselines/smcdiffopt/compute_scores.py --task $TASK --method $METHOD
 
 
 
